86b75a4fa6e6f4efe42d852b4893291749bca10f
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2021-09-21 01:28:32 +0800

    更新 UserDetail数据类型

--- types/api_UserDetail.go
+++ types/api_UserDetail.go
@@ -2,7 +2,31 @@ package types
 
 // UserDetailData 用户详细信息数据
 type UserDetailData struct {
-	RawJson     string
+	RawJson string
+
+	Identify struct {
+		ImageUrl  string      `json:"imageUrl"`
+		ImageDesc string      `json:"imageDesc"`
+		ActionUrl interface{} `json:"actionUrl"`
+	} `json:"identify"`
+	CurrentExpert struct {
+		RoleId   int         `json:"roleId"`
+		RoleName string      `json:"roleName"`
+		Level    interface{} `json:"level"`
+	} `json:"currentExpert"`
+	ExpertArray []struct {
+		RoleId   int         `json:"roleId"`
+		RoleName string      `json:"roleName"`
+		Level    interface{} `json:"level"`
+	} `json:"expertArray"`
+	CurrentProduct struct {
+		ProductionTypeName string `json:"productionTypeName"`
+		ProductionTypeId   int    `json:"productionTypeId"`
+	} `json:"currentProduct"`
+	Products []struct {
+		ProductionTypeName string `json:"productionTypeName"`
+		ProductionTypeId   int    `json:"productionTypeId"`
+	} `json:"products"`
 	Level       int `json:"level"`
 	ListenSongs int `json:"listenSongs"`
 	UserPoint   struct {
@@ -16,21 +40,28 @@ type UserDetailData struct {
 	MobileSign bool `json:"mobileSign"`
 	PcSign     bool `json:"pcSign"`
 	Profile    struct {
-		AvatarDetail       interface{} `json:"avatarDetail"`
-		AvatarImgIdStr     string      `json:"avatarImgIdStr"`
+		AvatarDetail struct {
+			UserType        interface{} `json:"userType"`
+			IdentityLevel   int         `json:"identityLevel"`
+			IdentityIconUrl string      `json:"identityIconUrl"`
+		} `json:"avatarDetail"`
 		BackgroundImgIdStr string      `json:"backgroundImgIdStr"`
 		UserId             int         `json:"userId"`
-		AvatarImgId        int64       `json:"avatarImgId"`
-		Birthday           int64       `json:"birthday"`
-		Gender             int         `json:"gender"`
-		Nickname           string      `json:"nickname"`
+		DjStatus           int         `json:"djStatus"`
 		AccountStatus      int         `json:"accountStatus"`
 		Province           int         `json:"province"`
 		VipType            int         `json:"vipType"`
 		Followed           bool        `json:"followed"`
-		UserType           int         `json:"userType"`
 		CreateTime         int64       `json:"createTime"`
-		AvatarUrl          string      `json:"avatarUrl"`
+		AvatarImgId        int64       `json:"avatarImgId"`
+		Birthday           int64       `json:"birthday"`
+		Gender             int         `json:"gender"`
+		Nickname           string      `json:"nickname"`
+		AvatarImgIdStr     string      `json:"avatarImgIdStr"`
+		Description        string      `json:"description"`
+		Mutual             bool        `json:"mutual"`
+		RemarkName         interface{} `json:"remarkName"`
+		UserType           int         `json:"userType"`
 		AuthStatus         int         `json:"authStatus"`
 		DetailDescription  string      `json:"detailDescription"`
 		Experts            struct {
@@ -40,51 +71,56 @@ type UserDetailData struct {
 		DefaultAvatar   bool        `json:"defaultAvatar"`
 		BackgroundImgId int64       `json:"backgroundImgId"`
 		BackgroundUrl   string      `json:"backgroundUrl"`
-		Mutual          bool        `json:"mutual"`
-		RemarkName      interface{} `json:"remarkName"`
-		Description     string      `json:"description"`
-		DjStatus        int         `json:"djStatus"`
+		AvatarUrl       string      `json:"avatarUrl"`
 		Signature       string      `json:"signature"`
 		Authority       int         `json:"authority"`
 		AllAuthTypes    []struct {
-			Type int         `json:"type"`
-			Desc string      `json:"desc"`
-			Tags interface{} `json:"tags"`
+			Type int      `json:"type"`
+			Desc string   `json:"desc"`
+			Tags []string `json:"tags"`
 		} `json:"allAuthTypes"`
 		Followeds                 int  `json:"followeds"`
 		Follows                   int  `json:"follows"`
 		Blacklist                 bool `json:"blacklist"`
+		ArtistId                  int  `json:"artistId"`
 		EventCount                int  `json:"eventCount"`
 		AllSubscribedCount        int  `json:"allSubscribedCount"`
 		PlaylistBeSubscribedCount int  `json:"playlistBeSubscribedCount"`
 		MainAuthType              struct {
-			Type int         `json:"type"`
-			Desc string      `json:"desc"`
-			Tags interface{} `json:"tags"`
+			Type int      `json:"type"`
+			Desc string   `json:"desc"`
+			Tags []string `json:"tags"`
 		} `json:"mainAuthType"`
-		FollowTime     string        `json:"followTime"`
-		FollowMe       bool          `json:"followMe"`
-		ArtistIdentity []interface{} `json:"artistIdentity"`
-		CCount         int           `json:"cCount"`
-		SDJPCount      int           `json:"sDJPCount"`
-		PlaylistCount  int           `json:"playlistCount"`
-		SCount         int           `json:"sCount"`
-		NewFollows     int           `json:"newFollows"`
+		AvatarImgIdStr1 string      `json:"avatarImgId_str"`
+		FollowTime      interface{} `json:"followTime"`
+		FollowMe        bool        `json:"followMe"`
+		ArtistIdentity  []int       `json:"artistIdentity"`
+		CCount          int         `json:"cCount"`
+		SDJPCount       int         `json:"sDJPCount"`
+		ArtistName      string      `json:"artistName"`
+		PlaylistCount   int         `json:"playlistCount"`
+		SCount          int         `json:"sCount"`
+		NewFollows      int         `json:"newFollows"`
 	} `json:"profile"`
 	PeopleCanSeeMyPlayRecord bool `json:"peopleCanSeeMyPlayRecord"`
 	Bindings                 []struct {
 		UserId       int         `json:"userId"`
+		Url          string      `json:"url"`
 		ExpiresIn    int         `json:"expiresIn"`
 		RefreshTime  int         `json:"refreshTime"`
 		BindingTime  int64       `json:"bindingTime"`
 		TokenJsonStr interface{} `json:"tokenJsonStr"`
 		Expired      bool        `json:"expired"`
-		Url          string      `json:"url"`
 		Id           int64       `json:"id"`
 		Type         int         `json:"type"`
 	} `json:"bindings"`
-	AdValid    bool  `json:"adValid"`
-	Code       int   `json:"code"`
-	CreateTime int64 `json:"createTime"`
-	CreateDays int   `json:"createDays"`
+	AdValid            bool  `json:"adValid"`
+	Code               int   `json:"code"`
+	CreateTime         int64 `json:"createTime"`
+	CreateDays         int   `json:"createDays"`
+	ProfileVillageInfo struct {
+		Title     string      `json:"title"`
+		ImageUrl  interface{} `json:"imageUrl"`
+		TargetUrl string      `json:"targetUrl"`
+	} `json:"profileVillageInfo"`
 }
