813c0878cd7e13f4678333f0fbcfb632eca8f4cd
xmxx <xibaole2333@gmail.com>
2022-03-12 22:50:16 +0800

    新增获取音乐人周任务API

--- api/musicianTasks.go
+++ api/musicianTasks.go
@@ -6,14 +6,31 @@ import (
 	"github.com/XiaoMengXinX/Music163Api-Go/utils"
 )
 
-// MusicianTasksAPI 获取音乐人任务列表 API
-const MusicianTasksAPI = "/api/nmusician/workbench/mission/cycle/list"
+// MusicianTasksDailyAPI 获取音乐人每日任务 API
+const MusicianTasksDailyAPI = "/api/nmusician/workbench/mission/cycle/list"
 
-// GetMusicianTasks 获取音乐人任务列表
-func GetMusicianTasks(data utils.RequestData) (result types.MusicianTasksData, err error) {
+// MusicianTasksWeeklyAPI 获取音乐人周任务 API
+const MusicianTasksWeeklyAPI = "/api/nmusician/workbench/mission/stage/list"
+
+// GetMusicianDailyTasks 获取音乐人每日任务
+func GetMusicianDailyTasks(data utils.RequestData) (result types.MusicianDailyTasksData, err error) {
 	var options utils.EapiOption
-	options.Path = MusicianTasksAPI
+	options.Path = MusicianTasksDailyAPI
 	options.Url = "https://music.163.com/eapi/nmusician/workbench/mission/cycle/list"
+	resBody, _, err := utils.ApiRequest(options, data)
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal([]byte(resBody), &result)
+	result.RawJson = resBody
+	return result, err
+}
+
+// GetMusicianWeeklyTasks 获取音乐人周任务
+func GetMusicianWeeklyTasks(data utils.RequestData) (result types.MusicianWeeklyTasksData, err error) {
+	var options utils.EapiOption
+	options.Path = MusicianTasksWeeklyAPI
+	options.Url = "https://music.163.com/eapi/nmusician/workbench/mission/stage/list"
 	resBody, _, err := utils.ApiRequest(options, data)
 	if err != nil {
 		return result, err
--- types/musicianTasks.go
+++ types/musicianTasks.go
@@ -1,7 +1,7 @@
 package types
 
-// MusicianTasksData 音乐人任务列表数据
-type MusicianTasksData struct {
+// MusicianDailyTasksData 音乐人每日任务列表
+type MusicianDailyTasksData struct {
 	RawJson string
 	Code    int    `json:"code"`
 	Message string `json:"message"`
@@ -31,6 +31,49 @@ type MusicianTasksData struct {
 			RewardType      int    `json:"rewardType"`
 			UserMissionId   int    `json:"userMissionId,omitempty"`
 			UpdateTime      int    `json:"updateTime,omitempty"`
+		} `json:"list"`
+	} `json:"data"`
+}
+
+// MusicianWeeklyTasksData 音乐人周任务列表
+type MusicianWeeklyTasksData struct {
+	RawJson string
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    struct {
+		List []struct {
+			Business            string `json:"business"`
+			MissionId           int    `json:"missionId"`
+			UserId              int    `json:"userId"`
+			MissionEntityId     int    `json:"missionEntityId"`
+			RewardId            int    `json:"rewardId"`
+			ProgressRate        int    `json:"progressRate"`
+			Description         string `json:"description"`
+			Type                int    `json:"type"`
+			Tag                 int    `json:"tag"`
+			ActionType          int    `json:"actionType"`
+			Platform            int    `json:"platform"`
+			Status              int    `json:"status"`
+			Button              string `json:"button"`
+			SortValue           int    `json:"sortValue"`
+			StartTime           int64  `json:"startTime"`
+			EndTime             int64  `json:"endTime"`
+			ExtendInfo          string `json:"extendInfo"`
+			Period              int    `json:"period"`
+			NeedToReceive       int    `json:"needToReceive"`
+			UserStageTargetList []struct {
+				SortValue    int `json:"sortValue"`
+				StageType    int `json:"stageType"`
+				Times        int `json:"times"`
+				Value        int `json:"value"`
+				ProgressRate int `json:"progressRate"`
+				SumTarget    int `json:"sumTarget"`
+				RewardId     int `json:"rewardId"`
+				RewardType   int `json:"rewardType"`
+				Worth        int `json:"worth"`
+				Status       int `json:"status"`
+			} `json:"userStageTargetList"`
+			HasSendWorth int `json:"hasSendWorth"`
 		} `json:"list"`
 	} `json:"data"`
 }
